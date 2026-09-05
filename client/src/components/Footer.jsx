850c8d8d7be669b8ce07a07eb6ec948d528c8c0a
Sumit-Kumar-Panigrahi <sumitkumarpanigrahi9278@gmail.com>
2026-09-05 19:53:59 +0530

    Created dashboard UI

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer className="w-full max-w-305 mx-auto bg-white/10 backdrop-blur xl:rounded-t-xl  px-6 py-4 text-center">
-      <p className="text-xs font-medium text-white">
+      <p className="text-xs font-medium text-slate-700">
         © 2026 Confera. All rights reserved. Built by Sumit 
       </p>
     </footer>
